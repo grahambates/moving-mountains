5621950a4b6387f80a815b130f0ffff4a2f0e72c
Graham Bates <info@grahambates.com>
2023-05-17 12:14:16 +0100

    WIP

--- mountains.lua
+++ mountains.lua
@@ -1,33 +1,27 @@
-m={}
-p={}
+u={}
+e={}
 TIC=function()
-  p=p+1
+  e=e+1
   for o=0,256 do
-    for e=0,136 do
-      pix(o,e,
-      	e<136-m[o+p//2]
-       and
-      	(m[(e*256+o+p//7)%256^2+1]+m[(e*256+o+p//4)%256^2+1])*e/756
-     -(o/2+e/4)%1
-      	or
-      	e<136-m[o+p]/2
-       and 1 or 0
-      )
+    for d=0,136 do
+      pix(o,d,(136-u[o+e//2]>d and d/632*(u[o+d*256]+u[o+e//3+d*256])or 136-u[o+e]/2>d and 2or 0)*(1-2^(-e
+/256))-(d/4+o/2)%1)
     end
-    poke(o%48+16320,256/(2^(5-o%48%3-o%48/5)+1))
+    poke(o%48+16320,256/(2^(5-o%48/5-o%48%3)+1))
   end
 end
 for o=0,5 do
-  for e=0,256^2 do
-    p[e]=math.random()
+  for d=0,3e5 do
+    e[d]=math.random()
   end
-  r=256//2^o
-  for e=0,256^2 do
-    d=p[e//2^o%r+e/256//2^o%r*r+1]+e%2^o/2^o*(p[(e//2^o%r+1)%r+e/256//2^o%r*r+1]-p[e//2^o%r+e/256//2^o%r*r
+  m=256//2^o
+  for d=0,3e5 do
+    p=e[d//2^o%m+d/256//2^o%m*m+1]+d%2^o/2^o*(e[(d//2^o%m+1)%m+d/256//2^o%m*m+1]-e[d//2^o%m+d/256//2^o%m*m
 +1])
-    m[e]=2^o*(e/256%2^o/2^o*(e%2^o/2^o*(p[(e//2^o%r+1)%r+(e/256//2^o%r+1)%r*r+1]-p[e//2^o%r+(e/256//2^o%r+
-1)%r*r+1])+p[e//2^o%r+(e/256//2^o%r+1)%r*r+1]-d)+d)+(m[e]or 0)
+    u[d]=(u[d]or 0)+2^o*(p+d/256%2^o/2^o*(d%2^o/2^o*(e[(d//2^o%m+1)%m+(d/256//2^o%m+1)%m*m+1]-e[d//2^o%m+(
+d/256//2^o%m+1)%m*m+1])+e[d//2^o%m+(d/256//2^o%m+1)%m*m+1]-p))
   end
 end
-p=1
-
+e=1
+-- *p/(256+p)
+--)*(math.sin(p/400)+1)
